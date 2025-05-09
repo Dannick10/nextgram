c21291d588e91278ba24d66793ab247ae4cd261b
Dannick010 <danmaggot2@gmail.com>
2025-05-09 16:30:02 -0300

    feat/config providers nextjs

--- auth.ts
+++ auth.ts
@@ -1,18 +1,40 @@
 import type { NextAuthConfig } from "next-auth";
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
-import google from "next-auth/providers/google"
+import google from "next-auth/providers/google";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const config = {
-    adapter: PrismaAdapter(prisma),
-    session: {strategy: "jwt"},
-    providers: [google],
-} satisfies NextAuthConfig
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [google],
+  callbacks: {
+    session({ session, token }) {
+      if (token.sub) session.user.userId = token.sub;
 
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/signIn"
+  },
+} satisfies NextAuthConfig;
 
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
+
+// providers
+interface ProviderWithId {
+    id: string;
+    name: string;
+}
+
+// map´providers
+
+export const providerMap = config.providers.map((provider) => {
+    const typedProvider = provider as unknown as ProviderWithId
+    return { id: typedProvider.id, name: typedProvider.name}
+})
